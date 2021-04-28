c19a92f4dfd59aaa8f022d7a8aee89857c64484c
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2021-04-28 20:33:24 +0100

    created a new file variable-and-data-types.js and a new file in Question-3

---
+++ Question-3/variables-and-data-types.js
@@ -0,0 +1,5 @@
+// created a file called variables-and-data-types.js
+// created a varaible and assigned the name of the Governor of imo state to the name variable. 
+// Log the variable name to the console.log()
+// created a variable called isAResidentOfVONGardenEstate and assign a boolean value confirm("Do you live in VON Garden estate")
+
---
+++ variables-and-data-types.js
@@ -0,0 +1,4 @@
+let name = 'Hope Uzodinma ';
+console.log(name);
+let isAResidentOfVONGardenEstate = confirm("Do you live in VON Garden estate");
+alert( isAResidentOfVONGardenEstate );
