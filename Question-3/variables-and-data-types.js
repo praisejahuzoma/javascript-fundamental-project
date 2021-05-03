6f6bdf62e011d446066c9118ffb3ad8f3a682faf
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2021-05-03 14:00:43 +0100

    added the question am working on as a comment at the beginning of my project.and also formatted my work properly

--- Question-3/variables-and-data-types.js
+++ Question-3/variables-and-data-types.js
@@ -1,6 +1,13 @@
+// Create a file called variables-and-data-types.js. 
+// In that file, on line 1 create a varaible and assign the name of the Governor of your state to that variable.
+// Log the variable name to the console using console.log()
+// On line 4, create a variable called isAResidentOfVONGardenEstate and assign a boolean value of your choice to it.
+
 // A varaible with the name of the Governor of imo state. 
 let name = 'Hope Uzodinma ';
+
 // Log the variable name to the console.log()
 console.log(name);
+
 // Create a variable called isAResidentOfVONGardenEstate and assign a boolean value 
 let isAResidentOfVONGardenEstate = true;
